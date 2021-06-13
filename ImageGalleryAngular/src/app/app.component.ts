b0e829c55596d804c8ec70483785162315b3bb93
sanjaysingh0017 <85580645+sanjaysingh0017@users.noreply.github.com>
2021-06-13 08:22:15 +0530

    slideshow added

--- ImageGalleryAngular/src/app/app.component.ts
+++ ImageGalleryAngular/src/app/app.component.ts
@@ -10,30 +10,45 @@ export class AppComponent {
   
   numbers: any;
   showFullView: boolean = false;
+  showSlideView: boolean = false;
   constructor() {
     this.numbers = Array(30).fill(1).map((x,i)=>i);
     console.log(this.numbers);
   }
-  src: string ='';
+  
+  srcFullView: string ='';
   openImage(src:string) {
-    this.src = src;
+    this.srcFullView = src;
     this.showFullView = true;
   }
   closeFullView() {
     this.showFullView = false;
     clearInterval(this.interval);
   }
+  closeSlideView() {
+    this.showSlideView = false;
+    clearInterval(this.interval);
+  }
   interval: any;
+  src1: string ='';
+  src2: string ='';
+  src3: string ='';
   startSlides() {
-    this.src = "assets/"+1+'.jpg';
-    this.showFullView = true;
-      var i=2;
+
+    this.showSlideView = true;
+      var i=0;
+      this.src1 = "assets/"+(i+30)+'.jpg';
+      this.src2 = "assets/"+(i+1)+'.jpg';
+      this.src3 = "assets/"+(i+2)+'.jpg';
       this.interval = setInterval( () => {
         if(i > 30) {
-          i = 1;
+          i = 0;
         }
-        this.src = "assets/"+i+'.jpg';
-          i++;
-      }, 2000, this );
+        i++;
+        this.src1 = "assets/"+(i)+'.jpg';
+        this.src2 = "assets/"+(i+1)+'.jpg';
+        this.src3 = "assets/"+(i+2)+'.jpg';
+          
+      }, 4000, this );
   }
 }
